8758610dcdc3ea0358353abf802260a849a0621a
Naveen <naveen@example.com>
2026-08-04 03:34:30 +0530

    add mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -4,10 +4,10 @@ LOGSDIR=/tmp
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
-R="\[31m"
-G="\[32m"
-N="\[0m"
-Y="\[33m"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 message=
 VALIDATE() {
     if [ $USERID -ne 0 ]
